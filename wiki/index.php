558ed10edb5c315653ba7b8da5688130db11a527
Johannes Schindelin <johannes.schindelin@gmx.de>
2013-04-05 09:15:17 -0500

    Use same trick as mediawiki/phase3/ for wiki/
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- wiki/index.php
+++ wiki/index.php
@@ -1,12 +1,33 @@
 <?
-apache_setenv( 'MW_INSTALL_PATH', '/var/www/vhosts/fiji.imagej.net/mediawiki/phase3/' );
-$_SERVER['SCRIPT_FILENAME'] = preg_replace( '/^\/var\/www\/vhosts\/fiji.imagej.net\/mediawiki\/phase3\/wiki\//', '/var/www/vhosts/fiji.imagej.net/mediawiki/phase3/', $_SERVER['SCRIPT_FILENAME'] );
-$_SERVER['REQUEST_URI'] = preg_replace( '/^\/wiki\//', '/', $_SERVER['REQUEST_URI'] );
-$_SERVER['SCRIPT_NAME'] = preg_replace( '/^\/wiki\//', '/', $_SERVER['SCRIPT_NAME'] );
-$_SERVER['SCRIPT_URL'] = preg_replace( '/^\/wiki\//', '/', $_SERVER['SCRIPT_URL'] );
+$path = $_SERVER['SCRIPT_FILENAME'];
+for (;;) {
+	$parent = preg_replace('/\/[^\/]*$/', '', $path);
+	if ($parent == $path) {
+		die("Internal error!");
+	}
+	if (file_exists($parent . '/LocalSettings.php')) {
+		break;
+	}
+	$path = $parent;
+}
+apache_setenv('MW_INSTALL_PATH', $parent . '/');
+$path = $_SERVER['SCRIPT_FILENAME'];
+$_SERVER['SCRIPT_FILENAME'] = $parent . '/index.php';
+if ($path == $_SERVER['SCRIPT_FILENAME']) {
+	die("Invalid path: $path");
+}
+
+$offset = strlen($_SERVER['SCRIPT_FILENAME']) - 10;
+$regex = '/^' . preg_replace('/\//', '\/', substr($path, $offset, strlen($path) - 10 - $offset)) . '\//';
+
+$_SERVER['REQUEST_URI'] = preg_replace($regex, '/', $_SERVER['REQUEST_URI']);
+$_SERVER['SCRIPT_NAME'] = preg_replace($regex, '/', $_SERVER['SCRIPT_NAME']);
+$_SERVER['SCRIPT_URL'] = preg_replace($regex, '/', $_SERVER['SCRIPT_URL']);
+
 if (!isset($_GET['title'])) {
         $_GET['title'] = 'Fiji';
 }
-require_once('/var/www/vhosts/fiji.imagej.net/mediawiki/phase3/index.php');
-//echo $_SERVER['SCRIPT_NAME'];
+
+require_once($_SERVER['SCRIPT_FILENAME']);
 ?>
+
