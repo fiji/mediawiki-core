e74145c5412f95e1012eee0ab80374ebae9580ab
Johannes Schindelin <johannes.schindelin@gmx.de>
2013-04-17 01:17:25 -0500

    Add the htpasswd function
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- extensions/SpecialChangeUploadPassword/ChangeUploadPassword.php
+++ extensions/SpecialChangeUploadPassword/ChangeUploadPassword.php
@@ -80,4 +80,41 @@ class SpecialChangeUploadPassword extends SpecialPage {
 
 		$wgOut->addHTML($this->getHTML());
 	}
+
+	function salt() {
+		return substr(str_shuffle('./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'), 0, 8);
+	}
+
+	function htpasswd($salt, $passwd) {
+		$len = strlen($passwd);
+		$text = $passwd . '$apr1$' . $salt;
+		$bin = pack('H32', md5($passwd . $salt . $passwd));
+		for ($i = $len; $i > 0; $i -= 16) {
+			 $text .= substr($bin, 0, min(16, $i));
+		}
+		for ($i = $len; $i > 0; $i >>= 1) {
+			$text .= ($i & 1) ? chr(0) : $passwd{0};
+		}
+		$bin = pack('H32', md5($text));
+		for ($i = 0; $i < 1000; $i++) {
+			$new = ($i & 1) ? $passwd : $bin;
+			if ($i % 3) $new .= $salt;
+			if ($i % 7) $new .= $passwd;
+			$new .= ($i & 1) ? $bin : $passwd;
+			$bin = pack('H32', md5($new));
+		}
+		for ($i = 0; $i < 5; $i++) {
+			$k = $i + 6;
+			$j = $i + 12;
+			if ($j == 16) {
+				$j = 5;
+			}
+			$tmp = $bin[$i] . $bin[$k] . $bin[$j] . $tmp;
+		}
+		$tmp = chr(0) . chr(0) . $bin[11] . $tmp;
+		$tmp = strtr(strrev(substr(base64_encode($tmp), 2)),
+			'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/',
+			'./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz');
+		return '$' . 'apr1' . '$' . $salt . '$' . $tmp;
+	}
 }
