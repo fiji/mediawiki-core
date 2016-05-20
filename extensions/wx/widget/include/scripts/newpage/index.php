7c654a4fa3239c49c7944c9614e89b8734fc098a
Curtis Rueden <ctrueden@wisc.edu>
2016-05-20 13:28:37 -0500

    Remove obsolete extensions
    These are all either:
    A) Managed properly as submodules from the toplevel; or
    B) No longer used anywhere, not maintained anymore, and/or discontinued.
    
    Specific comments on each:
    
    == Add_HTML_Meta_and_Title ==
    Now managed as a submodule from the toplevel.
    
    == BibTex ==
    Only used on one page: Generate_and_exploit_Kymographs.
    Will update that page to use Citation template instead.
    
    == Calendar ==
    Unused, as far as we can tell.
    The Dscho subdirectory was a customized/specialized version.
    The toplevel manages Calendar as a submodule, but we don't need it.
    
    == FijiRandomSnapshot ==
    No longer used. The snapshots are now displayed at http://fiji.sc/
    outside the wiki, via a Bootstrap.js slideshow.
    
    == FlashSwf ==
    Security risk and not maintained.
    Plays .swf files.
    Syntax <swf>
    Used on a few pages, will need to be replaced.
    No viable extensions with exact same behavior found,
    though FlvHandler might work.
    
    == ParserFunctions ==
    Now managed as a submodule from the toplevel.
    
    == RSS ==
    Not used anywhere.
    Syntax <rss>
    Displays RSS feeds on wiki pages
    NewsChannel provides RSS for the News items (we think...).
    
    == flvplayer ==
    This is a security risk.
    Allows for display of flv movies in wiki.
    Syntax <flvplayer>
    TODO: Move all pages that use this extension to a new solution.
    Possible replacement extension FlvHandler:
    https://www.mediawiki.org/wiki/Extension:FlvHandler
    
    == include ==
    This is a security risk.
    Syntax <include iframe...>
    Primarily used to iframe youtube videos.
    Replace with already installed EmbedVideo, syntax {{#ev:...}}
    Example usage of EmbedVideo: http://wiki.imagej.net/Hackathon
    This was also used to iframe the Fiji map, which is a bing map.
    TODO: Find a secure solution for the Fiji map.
    
    == wx ==
    a.k.a. WikiWidgets
    Not actively maintained.
    Allows for embedding information from other source into wiki page.
    Usage unknown.
    Seems to have been replaced by Widgets extension (which is already
    in use on site): https://www.mediawiki.org/wiki/Extension:Widgets
    
    Remove ParserFunctions extension
    
    It has its own submodule now.

--- extensions/wx/widget/include/scripts/newpage/index.php
+++
@@ -1,8 +0,0 @@
-<script>
-    function newPage() {
-        var page = prompt('New Page', 'Page Title');
-        page = page.replace(/\s+/, '_');
-        window.location = 'http://hexten.net/wiki/index.php/' + page;
-    }
-</script>
-<a href="javascript:newPage()">New Page</a>
--- extensions/wx/widget/include/scripts/test/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $widget = wxCurrentWidget();
-
-    $title = $widget->arg('title', 'No title');
-
-    print "$title";
-?>
